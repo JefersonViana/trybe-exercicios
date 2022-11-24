fd62152460c71ec729a1678683015d3663ea0567
jefersonviana <jefersonv28@gmail.com>
2022-11-24 13:11:23 -0300

    realizando exercicios do dia

---
+++ 01.Fundamentos/secao-04-introducao-a-javascript/Dia-01:JavaScript:Primeiros:Primeiros-passos/exercicio-1/script.js
@@ -0,0 +1,74 @@
+// parte - 1 //
+
+// const a = 7;
+// const b = 5;
+
+// const adicao = a + b;
+// const subtracao = a - b;
+// const multiplicacao = a * b;
+// const divisao = a / b;
+// const modulo = a % b;
+
+// console.log(adicao);
+// console.log(subtracao);
+// console.log(multiplicacao);
+// console.log(divisao);
+// console.log(modulo);
+
+// concluido//
+
+
+// parte - 2 //
+
+// const valor1 = 49;
+// const valor2 = 50;
+
+// if (valor1 > valor2) {
+//   console.log('O menor valor é valor2')
+// }else if (valor2 > valor1) {
+//   console.log('O menor valor é valor1');
+// }else {
+//   console.log('Os valores são iguais');
+// }
+
+// concluido //
+
+
+// parte - 3 //
+
+const value1 = 21;
+const value2 = 21;
+const value3 = 21;
+
+if (value1 > value2 && value1 > value3 && value1 !== value2 && value1 !== value3) {
+  console.log(value1);
+
+} else if (value1 === value2 && value1 > value3) {
+  console.log('value1 ' + value1 + ' e ' + 'value2 ' + value2 + ' são iguas e maiores que o value3 que é ' + value3);
+
+}else if (value1 === value3 && value1 > value2) {
+  console.log('value1 ' + value1 + ' e ' + 'value3 ' + value3 + ' são iguais e maiores que o value2 que é ' + value2);
+
+} else if (value2 > value1 && value2 > value3 && value2 !== value1 && value2 !== value3) {
+  console.log(value2);
+
+} else if (value2 === value1 && value2 > value3) {
+  console.log('value2 ' + value2 + ' e ' + 'value1' + value1 + ' são iguas e maiores que o value3 que é ' + value3);
+
+}else if (value2 === value3 && value2 > value1) {
+  console.log('value2 ' + value2 + ' e ' + 'value3 ' + value3 + ' são iguais e maiores que o value1 que é ' + value1);
+
+}else if(value3 > value1 && value3 > value2) {
+  console.log(value3);
+
+} else if (value3 === value1 && value3 > value2) {
+  console.log('value3 ' + value3 + ' e ' + 'value1 ' + value1 + ' são iguas e maiores que o value2 que é ' + value2);
+
+} else if (value3 === value2 && value3 > value1) {
+  console.log('value3 ' + value3 + ' e ' + 'value2 ' + value2 + ' são iguas e maiores que o value1 que é ' + value1);
+
+} else {
+  console.log('Todos os valores são iguais');
+}
+
+// concluido //
