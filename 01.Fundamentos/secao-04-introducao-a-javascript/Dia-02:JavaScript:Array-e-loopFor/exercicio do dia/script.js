108a0fa83e840fdf0bbf610cc36e050a10145b40
jefersonviana <jefersonv28@gmail.com>
2022-11-28 22:27:59 -0300

    exercicio do dia concluido

---
+++ 01.Fundamentos/secao-04-introducao-a-javascript/Dia-02:JavaScript:Array-e-loopFor/exercicio do dia/script.js
@@ -0,0 +1,48 @@
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// let higherNumber = numbers[0];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   if (numbers[index] < higherNumber) {
+//     higherNumber = numbers[index];
+//   }
+// }
+
+// console.log(higherNumber);
+
+
+// parte 2 //
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 !== 0) {
+//     result += 1;
+//   }
+// }
+
+// if (result === 0) {
+//   console.log('nenhum valor ímpar encontrado');
+// } else {
+//   console.log(result);
+// }
+
+///////////////////////
+
+// parte 3 ///
+
+// let numbers = [];
+
+// for (let index = 1; index <= 25; index += 1) {
+//   numbers.push(index);
+// }
+
+// for (let index1 = 0; index1 < numbers.length; index1 += 1) {
+//   console.log(numbers[index1] / 2);
+// }
+
+
+//////////////
+
+
