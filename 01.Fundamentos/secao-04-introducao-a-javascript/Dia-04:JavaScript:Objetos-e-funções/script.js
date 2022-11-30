cc75a7abd4153dceaeec12cc53c1f7046e9c6799
jefersonviana <jefersonv28@gmail.com>
2022-11-30 16:23:07 -0300

    realizando tarefas do dia

---
+++ 01.Fundamentos/secao-04-introducao-a-javascript/Dia-04:JavaScript:Objetos-e-funções/script.js
@@ -0,0 +1,81 @@
+// parte 1//
+
+// let car = {
+//   type: 'Fiat',
+//   model: '500',
+//   color: 'white',
+// };
+
+// console.log(car.model);
+
+///////////
+
+/// parte 2 ////
+
+// let diasDaSemana = {
+//   1: 'domingo',
+//   2: 'segunda',
+//   3: 'terça',
+//   4: 'quarta',
+//   5: 'quinta',
+//   6: 'sexta',
+//   7: 'sábado',
+// };
+
+// diasDaSemana.1; <<<<<< não pode usar esse tipo se a chave for number
+// console.log(diasDaSemana[6]);
+/////////////////////////////
+
+
+// parte 3 ////
+
+
+// let conta = {
+//   agencia: '0000',
+//   banco: {
+//     cod: '012',
+//     id: 4,
+//     nome: 'TrybeBank',
+//   },
+// };
+
+// let infoDoBanco = 'banco';
+// console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+// console.log(conta[infoDoBanco]['nome']); // TrybeBank
+
+// console.log(conta.agencia); // 0000
+// console.log(conta['agencia']); // 0000
+
+// console.log(conta.banco.cod); // 012
+// console.log(conta['banco']['id']); // 4
+
+
+// parte 4 ///
+
+
+// let usuario = {
+//   id: 99,
+//   email: 'jakeperalta@gmail.com',
+//   infoPessoal: {
+//     nome: 'Jake',
+//     sobrenome: 'Peralta',
+//     endereco: {
+//       rua: 'Smith Street',
+//       bairro: 'Brooklyn',
+//       cidade: 'Nova Iorque',
+//       estado: 'Nova Iorque',
+//     },
+//   },
+// };
+
+// console.log(usuario['id']); // 99
+// console.log(usuario.email); // jakeperalta@gmail.com
+
+// console.log(usuario.infoPessoal.endereco.rua); // Smith Street
+// console.log(usuario['infoPessoal']['endereco']['cidade']); // Nova Iorque
+
+
+////////////////////
+
+
+
