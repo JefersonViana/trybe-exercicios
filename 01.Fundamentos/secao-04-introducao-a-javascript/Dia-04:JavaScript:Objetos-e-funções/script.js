3af50e5a56e7ebbf49ee5324153a619ac4a0b984
jefersonviana <jefersonv28@gmail.com>
2022-12-01 10:12:45 -0300

    exercicios até a parte 7, feitos

--- 01.Fundamentos/secao-04-introducao-a-javascript/Dia-04:JavaScript:Objetos-e-funções/script.js
+++ 01.Fundamentos/secao-04-introducao-a-javascript/Dia-04:JavaScript:Objetos-e-funções/script.js
@@ -77,5 +77,68 @@
 
 ////////////////////
 
+// parte 5 ////
+
+
+
+// let moradores = [
+//   {
+//     nome: 'Luiza',
+//     sobrenome: 'Guimarães',
+//     andar: 10,
+//     apartamento: 1005,
+//   },
+//   {
+//     nome: 'William',
+//     sobrenome: 'Albuquerque',
+//     andar: 5,
+//     apartamento: 502,
+//   },
+//   {
+//     nome: 'Murilo',
+//     sobrenome: 'Ferraz',
+//     andar: 8,
+//     apartamento: 804,
+//   },
+//   {
+//     nome: 'Zoey',
+//     sobrenome: 'Brooks',
+//     andar: 1,
+//     apartamento: 101,
+//   },
+// ];
+
+// let primeiroMorador = moradores[0];
+// console.log(primeiroMorador); // { nome: 'Luiza', sobrenome: 'Guimarães', andar: 10, apartamento: 1005 }
+// console.log(primeiroMorador['andar']); // 10
+
+// let ultimoMorador = moradores[moradores.length - 1];
+// console.log(ultimoMorador); // { nome: 'Zoey', sobrenome: 'Brooks', andar: 1, apartamento: 101 }
+// console.log(ultimoMorador['apartamento']); // Zoey
+
+//////////////////////////
+
+// parte 6 ////
+
+
+// let player = {
+//   name: 'Marta',
+//   lastName: 'Silva',
+//   age: 34,
+//   medals: {
+//     golden: 2,
+//     silver: 3,
+//   }
+// }
+
+
+// console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade');
+
+/////////////////////////////////////
+
+
+
+// parte 7 ///
+
 
 
