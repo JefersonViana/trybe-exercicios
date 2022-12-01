4189ceea7952e7d7139bb2de1c4b19063b7399b4
jefersonviana <jefersonv28@gmail.com>
2022-12-01 11:05:25 -0300

    terminando objetivos

--- 01.Fundamentos/secao-04-introducao-a-javascript/Dia-04:JavaScript:Objetos-e-funções/script.js
+++ 01.Fundamentos/secao-04-introducao-a-javascript/Dia-04:JavaScript:Objetos-e-funções/script.js
@@ -1,3 +1,5 @@
+// OBJETOS //
+
 // parte 1//
 
 // let car = {
@@ -130,15 +132,16 @@
 //     silver: 3,
 //   }
 // }
-
+// player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+// let melhorDoMundo = player.bestInTheWorld.length;
 
 // console.log('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade');
+// console.log('A jogadora ' + player['name'] + ' ' + player['lastName'] + ' foi eleita a melhor do mundo por ' + melhorDoMundo + ' vezes');
+// console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata');
 
-/////////////////////////////////////
+// FIM DE OBJETOS ///
 
-
-
-// parte 7 ///
+// INICIO DE FOR/IN E FOR/OF //
 
 
 
