b228deb40d1edff33b0ad452e985877411ef4fc2
jefersonviana <jefersonv28@gmail.com>
2022-12-01 18:56:48 -0300

    Exemplos de arrow fuctions

--- 01.Fundamentos/secao-04-introducao-a-javascript/Dia-05:JavaScript-ES6:let,const,arrow-functions-e-template-literals/script.js
+++ 01.Fundamentos/secao-04-introducao-a-javascript/Dia-05:JavaScript-ES6:let,const,arrow-functions-e-template-literals/script.js
@@ -1,9 +1,46 @@
 
 // Iniciando arrow fuctions //
+// Exemplo 1 //
 
-function printName() {
-  const myName = 'Lucas';
-  return myName;
-};
+// function printName() {
+//   const myName = 'Lucas';
+//   return myName;
+// };
 
-console.log(printName());
+// console.log(printName());
+
+// Exemplo 2 //
+
+// const printName = function () {
+//   const myName = 'Lucas';
+//   return myName;
+// };
+
+// console.log(printName());
+
+// Exemplo 3 //
+
+// const printName = () => {
+//   const myName = 'Lucas';
+//   return myName;
+// };
+
+// console.log(printName());
+
+ // Exemplo 4 //
+
+// const printName = () => 'Lucas';
+// console.log(printName());
+
+// Exemplo 5 //
+
+// const multiplyByTwo = number => number * 2;
+// console.log(multiplyByTwo(10));
+
+// Exemplo 6 //
+
+// const multiplication = (number, multi) => number * multi;
+// console.log(multiplication(8, 2));
+
+// Exemplo 7 //
+
