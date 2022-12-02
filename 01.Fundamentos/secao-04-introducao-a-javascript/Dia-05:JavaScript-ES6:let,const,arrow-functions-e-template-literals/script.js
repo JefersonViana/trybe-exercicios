ed4ad9541a7a3a9899ccf7f6cdd284e03561f525
jefersonviana <jefersonv28@gmail.com>
2022-12-01 21:28:57 -0300

    acrescetando array

--- 01.Fundamentos/secao-04-introducao-a-javascript/Dia-05:JavaScript-ES6:let,const,arrow-functions-e-template-literals/script.js
+++ 01.Fundamentos/secao-04-introducao-a-javascript/Dia-05:JavaScript-ES6:let,const,arrow-functions-e-template-literals/script.js
@@ -44,3 +44,30 @@
 
 // Exemplo 7 //
 
+// Exercicios //
+
+// Parte 1 //
+
+// function imprimeIdade() {
+//   for (let idade = 30; idade <= 40; idade += 1) {
+//     console.log('Idade dentro do for:', idade)
+//   }
+// }
+// imprimeIdade()
+
+// parte 2 //
+
+  // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+  // const pessoa = {
+  //   nome: 'Henri',
+  //   idade: 20
+  // }
+
+  // pessoa.nome = 'luna';
+  // pessoa.idade = 19;
+
+  // console.log('Nome:', pessoa.nome);
+  // console.log('Idade:', pessoa.idade);
+
+
+  const array = [1, 2, 3, 4, 5];
