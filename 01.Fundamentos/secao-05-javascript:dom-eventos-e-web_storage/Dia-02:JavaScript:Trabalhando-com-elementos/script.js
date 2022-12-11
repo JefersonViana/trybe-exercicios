5cab9452766df3d6805b46b28424bd25086ffa68
jefersonviana <jefersonv28@gmail.com>
2022-12-11 20:05:47 -0300

    Finalizando Exercícios de aprendizado.

--- 01.Fundamentos/secao-05-javascript:dom-eventos-e-web_storage/Dia-02:JavaScript:Trabalhando-com-elementos/script.js
+++ 01.Fundamentos/secao-05-javascript:dom-eventos-e-web_storage/Dia-02:JavaScript:Trabalhando-com-elementos/script.js
@@ -1,19 +1,4 @@
-const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+let elementoPai = document.getElementById("elementoOndeVoceEsta");
+elementoPai.removeChild(elementoPai.firstElementChild);
+console.log(elementoPai);
 
-const pai = elementoOndeVoceEsta.parentElement;
-pai.style.color = 'blue';
-pai.style.fontSize = '50px'
-console.log(pai);
-const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
-primeiroFilhoDoFilho.innerText = 'Estou entendendo!';
-console.log(primeiroFilhoDoFilho);
-const primeiroFilho = pai.firstElementChild;
-console.log(primeiroFilho);
-const primeiroFilho2 = elementoOndeVoceEsta.previousElementSibling;
-console.log(primeiroFilho2);
-const textElement = elementoOndeVoceEsta.nextSibling;
-console.log(textElement);
-const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
-console.log(terceiroFilho);
-const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
-console.log(terceiroFilho2);
